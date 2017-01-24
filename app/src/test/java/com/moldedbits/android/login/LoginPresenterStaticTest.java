23a3113b098626d8e4b62cda7fef085693f76e37
Anuj <anuj.middha@gmail.com>
2017-01-24 20:18:01 +0530

    added missing RunWith

--- app/src/test/java/com/moldedbits/android/login/LoginPresenterStaticTest.java
+++ app/src/test/java/com/moldedbits/android/login/LoginPresenterStaticTest.java
@@ -4,15 +4,20 @@ import com.moldedbits.android.utils.StaticUtils;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( { StaticUtils.class })
 public class LoginPresenterStaticTest {
     @Mock
     LoginView mLoginView;
